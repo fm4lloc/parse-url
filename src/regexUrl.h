fd223f7c0e9afa5d5bea579f20c4e1829bf5dcde
Fernando Magalhães <fm4lloc@gmail.com>
2015-02-02 16:44:59 -0200

    Update regexUrl.h

--- src/regexUrl.h
+++ src/regexUrl.h
@@ -12,17 +12,17 @@
 #define REGEXURL_H_
 
 /* Code index */
-#define 	SCHEME 		(2)
-#define 	AUTHORITY 	(4)
-#define 	USERINFO 	(6)
-#define 	USERNAME 	(7)
-#define 	PASSWORD 	(9)
-#define 	HOST		(10)
-#define 	PORT 		(13)
-#define 	PATH 		(14)
-#define 	FILENAME 	(17)
-#define 	QUERY 		(19)
-#define 	FRAGMENT 	(21)
+#define 	SCHEME      (2)
+#define 	AUTHORITY   (4)
+#define 	USERINFO    (6)
+#define 	USERNAME    (7)
+#define 	PASSWORD    (9)
+#define 	HOST        (10)
+#define 	PORT        (13)
+#define 	PATH        (14)
+#define 	FILENAME    (17)
+#define 	QUERY       (19)
+#define 	FRAGMENT    (21)
 
 /* Scheme */
 #define SCHEME_ER 		"^(([^:/?#]+):)?"
@@ -31,8 +31,8 @@
 #define HOSTNAME_ER		"[^:/?#@]*"
 #define IPV6REF_ER		"\\[([0-9a-fA-F:]+)\\]"
 
-#define HOST_ER			"(" IPV6REF_ER "|" HOSTNAME_ER ")"
-#define PORT_ER			"(:([0-9]+))?"
+#define HOST_ER       "(" IPV6REF_ER "|" HOSTNAME_ER ")"
+#define PORT_ER			  "(:([0-9]+))?"
 
 #define HOSTPORT_ER	HOST_ER PORT_ER
 
@@ -45,13 +45,13 @@
 #define AUTHORITY_ER	"(//("  USERINFO_ER HOSTPORT_ER "))?"
 
 /* Path */
-#define PATH_ER			"([^?#]*)"
+#define PATH_ER       "([^?#]*)"
 #define FILENAME_ER		"(/([^?#]*))"
 
 #define HIER_PART_ER	"(" PATH_ER FILENAME_ER ")?"
 
 /* Query and fragments */
-#define QUERY_ER		"(\\?([^#]*))?"
+#define QUERY_ER      "(\\?([^#]*))?"
 #define FRAGMENT_ER		"(#(.*))?$"
 
 /* URL */
